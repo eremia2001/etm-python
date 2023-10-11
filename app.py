fa7e3a2720deee7668c22507ac10e47e8054fe95
Eremiya Rifat <eremia2021@gmail.com>
2023-10-11 18:27:28 +0200

    geordnet

--- app.py
+++ app.py
@@ -6,22 +6,18 @@ from create_structured_csv import create_structured_csv
 from create_structured_KTL import create_structured_ktl
 
 def main():
-    # Erstellen Sie das Hauptfenster
     root = tk.Tk()
     root.title("CSV zu JSON Konverter")
 
-    # Haupt-Frame
     main_frame = Frame(root, padx=20, pady=20)
     main_frame.pack(padx=10, pady=10, expand=True, fill="both")
 
-    # Anleitung für den Benutzer
     instruction_font = font.Font(size=10, weight="bold")
-    instruction_label = Label(main_frame, text="1.Klicken Sie auf den Button, um eine CSV-Datei auszuwählen.\n"
-                                               "2.den Speicherort und Namen auswhählen.",
+    instruction_label = Label(main_frame, text="1. Klicken Sie auf den Button, um eine CSV-Datei auszuwählen.\n"
+                                               "2. Den Speicherort und Namen auswählen.",
                               font=instruction_font)
     instruction_label.pack(pady=10)
 
-    # Funktion zum Anzeigen der Fehlermeldung in einem neuen Fenster
     def show_error(error_message):
         error_window = Toplevel(root)
         error_window.title("Fehlermeldung")
@@ -34,8 +30,7 @@ def main():
         scrollbar.pack(side="right", fill="y")
         text_area.config(yscrollcommand=scrollbar.set)
 
-    # Funktion zum Öffnen der CSV-Datei
-    def open_file():
+    def open_file(conversion_function):
         try:
             file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
             if not file_path:
@@ -47,20 +42,21 @@ def main():
             remove_spaces(file_path)
             data = convert_csv_to_json(file_path,"balbajs2.json")
             
-            # Den Benutzer fragen, wohin die Datei gespeichert werden soll
             save_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
             
             if not save_path:
                 raise ValueError("Kein Speicherort ausgewählt!")
             
-            create_structured_ktl(data, save_path)
+            conversion_function(data, save_path)
         except Exception as e:
             show_error(str(e))
 
-    # Erstellen Sie einen Button, um die CSV-Datei zu öffnen
     btn_font = font.Font(size=12)
-    open_btn = tk.Button(main_frame, text="CSV Datei öffnen", command=open_file, font=btn_font, bg="#4CAF50", fg="white", padx=10, pady=5)
-    open_btn.pack(pady=20)
+    open_csv_btn = tk.Button(main_frame, text="CSV Datei öffnen (CSV)", command=lambda: open_file(create_structured_csv), font=btn_font, bg="#4CAF50", fg="white", padx=10, pady=5)
+    open_csv_btn.pack(pady=20)
+
+    open_ktl_btn = tk.Button(main_frame, text="CSV Datei öffnen (KTL)", command=lambda: open_file(create_structured_ktl), font=btn_font, bg="#4CAF50", fg="white", padx=10, pady=5)
+    open_ktl_btn.pack(pady=20)
 
     root.mainloop()
 
